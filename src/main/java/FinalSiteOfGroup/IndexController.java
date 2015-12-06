d785b37214feb95ec441efd4b33428fc882324ea
andrey56097 <batsandrey@gmail.com>
2015-12-06 23:10:41 +0200

    change in classes,  adding request mapping

--- src/main/java/FinalSiteOfGroup/IndexController.java
+++ src/main/java/FinalSiteOfGroup/IndexController.java
@@ -4,6 +4,7 @@ package FinalSiteOfGroup;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,38 +14,63 @@ import org.springframework.web.bind.annotation.RequestParam;
 import FinalSiteOfGroup.models.Post;
 import FinalSiteOfGroup.services.PostsService;
 
+import FinalSiteOfGroup.models.User;
+import FinalSiteOfGroup.services.UsersService;
+
 @Controller
 public class IndexController {
-
 	 @Autowired
-     private PostsService postsService;
+	  private PostsService postsService;
 
-     @RequestMapping("/About us")
-     public String index(Model model) {
-         model.addAttribute("posts", postsService.getRecentPosts());
-         return "About us";
-     }
-     
-     @RequestMapping(value = "/post", method = RequestMethod.POST)
-     public String createPost(@RequestParam("text") String postText) {
-         postsService.addPost(new Post("Unknown", postText, new Date()));
-         return "redirect:About us";
-     }
-     
-     @RequestMapping("/Home")
-     public String index() {
-         return "Home";
-     }
-     
-     @RequestMapping("/Cathalogs")
-     public String index2() {
-         return "Cathalogs";
-     }
-     
-     @RequestMapping("/Contacts")
-     public String index3() {
-         return "Contacts";
-     }
-    
-}     
+	  @Autowired
+	  private UsersService usersService;
+
+	  @RequestMapping("/")
+	  public String index(Model model) { return "index"; }
+
+	  @RequestMapping("/About us")
+	  public String home(Model model, @RequestParam(value = "page", defaultValue = "1") int page) {
+	    Page<Post> postsPage = postsService.getPosts(page, 5);
+	    model.addAttribute("posts", postsPage.getContent());
+	    model.addAttribute("users", usersService.getSubscribeRecommendations());
+	    model.addAttribute("pagesCount", postsPage.getTotalPages());
+	    model.addAttribute("currentPage", page);
+	    return "About us";
+	  }
+
+	  @RequestMapping(value = "/post", method = RequestMethod.POST)
+	  public String createPost(@RequestParam("text") String postText) {
+	    postsService.addPost(postText);
+	    return "redirect:About us";
+	  }
+
+	  @RequestMapping(value = "/register", method = RequestMethod.POST)
+	  public String register(@RequestParam("login") String login, 
+	      @RequestParam("email") String email, 
+	      @RequestParam("pass") String pass) { 
+	    usersService.register(login, email, pass);
+	    return "redirect:home";
+	  }
+
+	  @RequestMapping(value="/subscribe", method = RequestMethod.POST)
+	  public String subscribe(@RequestParam("login") String login) {
+	    usersService.subscribe(login);
+	    return "redirect:home";
+	  }
+	  
+	  @RequestMapping("/Home")
+	    public String index() {
+	        return "Home";
+	    }
+
+	    @RequestMapping("/Cathalogs")
+	    public String index2() {
+	        return "Cathalogs";
+	    }
+
+	    @RequestMapping("/Contacts")
+	    public String index3() {
+	        return "Contacts";
+	    }
+} 
 
--- src/main/java/FinalSiteOfGroup/models/Post.java
+++ src/main/java/FinalSiteOfGroup/models/Post.java
@@ -1,36 +1,68 @@
 package FinalSiteOfGroup.models;
 
-import java.util.Date;
+import java.util.*;
 
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+@Entity
+@Table(name = "posts")
 public class Post {
-	 private String author;
-     private String text;
-     private Date createdAt;
-     
-     public Post(String author, String text, Date createdAt) {
- 		super();
- 		this.author = author;
- 		this.text = text;
- 		this.createdAt = createdAt;
- 	}
-	public String getAuthor() {
+	
+	@Id
+	@GeneratedValue
+    private Long id;
+	
+	@NotNull
+	@ManyToOne
+	 private User author;
+    
+	@NotBlank
+    @Size(min = 1, max = 2048)
+    private String text;
+    
+	@NotNull
+    private Date createdAt;
+    
+	public Post(User author, String text, Date createdAt) {
+		this.author = author;
+		this.text = text;
+		this.createdAt = createdAt;
+	}
+
+	public Post() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public User getAuthor() {
 		return author;
 	}
-	public void setAuthor(String author) {
+
+	public void setAuthor(User author) {
 		this.author = author;
 	}
+
 	public String getText() {
 		return text;
 	}
+
 	public void setText(String text) {
 		this.text = text;
 	}
+
 	public Date getCreatedAt() {
 		return createdAt;
 	}
+
 	public void setCreatedAt(Date createdAt) {
 		this.createdAt = createdAt;
 	}
-     
-     
 }
